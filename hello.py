864d45ec59a135f8f235542ecfc147d824e5bc2f
newtonthapa35 <newtonthapa35@gmail.com>
2024-10-18 14:18:43 +0200

    stashed to stash

--- hello.py
+++ hello.py
@@ -1,5 +1,17 @@
 print("hello world to this beautiful world")
 print("hello world to this beautiful world2")
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+print("hello world to this beautiful world3")
+
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+print("hello world to this beautiful world4")
 
 
 
